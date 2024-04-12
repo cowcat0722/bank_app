04bee7a060424f1719d72e1fbe6d8922b0ac3dad
Pakchan <1625787@donga.ac.kr>
2024-04-12 20:55:13 +0900

    계좌 상세보기

--- src/main/java/com/example/bank/account/AccountController.java
+++ src/main/java/com/example/bank/account/AccountController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,15 @@ public class AccountController {
     private final AccountService accountService;
     private final HttpSession session;
 
+    @GetMapping("/api/accounts/{number}")
+    public ResponseEntity<?> accountDetail(@PathVariable Integer number) {
+        SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
+        AccountResponse.DetailDTO respDTO = accountService.계좌상세보기(number, sessionUser);
+        return ResponseEntity.ok(new ApiUtil(respDTO));
+    }
+
     @GetMapping("/api/accounts")
-    public ResponseEntity<?> accountList(){
+    public ResponseEntity<?> accountList() {
         SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
         AccountResponse.MainDTO respDTO = accountService.계좌목록보기(sessionUser.getId());
         return ResponseEntity.ok(new ApiUtil(respDTO));
